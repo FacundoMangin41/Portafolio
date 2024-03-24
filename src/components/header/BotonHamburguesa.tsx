c180b38f8426fbf50d00d9ac08fe5f49002d2184
Facundo Mangin <63527441+FacundoMangin41@users.noreply.github.com>
2024-03-24 20:50:34 -0300

    Arreglo redirecciones y sidebar

--- src/components/header/BotonHamburguesa.tsx
+++ src/components/header/BotonHamburguesa.tsx
@@ -12,27 +12,26 @@ import MenuIcon from '@mui/icons-material/Menu';
 
 import './header.css'
 
+import perfil from "../../assets/perfil.jpg";
 
 import { Link } from 'react-router-dom';
-import { drawerItems, drawerItemsAyuda } from "./RedireccionesLayout";
-import { drawerItemsReportes } from "./RedireccionesLayout";
-import { drawerItemsRentas } from "./RedireccionesLayout";
+import { drawerItems } from "./RedireccionesLayout";
 import { iconMap } from "./RedireccionesLayout";
 
 
 interface DrawerItem {
-    label: string;
-    path: string;
-  }
-  
-  interface Props {
-      drawerItemsReportes: DrawerItem[]; // Suponiendo que drawerItemsReportes es una matriz de objetos con propiedades label y path
-      drawerItems: DrawerItem[]; // Suponiendo que drawerItems es una matriz de objetos con propiedades label y path
-      drawerItemsRelevamientos: DrawerItem[]; // Suponiendo que drawerItemsRelevamientos es una matriz de objetos con propiedades label y path
-      drawerItemsRentas: DrawerItem[]; // Suponiendo que drawerItemsRentas es una matriz de objetos con propiedades label y path
-      drawerItemsAyuda: DrawerItem[]; // Suponiendo que drawerItemsAyuda es una matriz de objetos con propiedades label y path
-      iconMap: { [key: string]: React.ReactNode }; // Suponiendo que iconMap es un objeto que asigna etiquetas a iconos React
-    }
+  label: string;
+  path: string;
+}
+
+interface Props {
+  drawerItemsReportes: DrawerItem[]; // Suponiendo que drawerItemsReportes es una matriz de objetos con propiedades label y path
+  drawerItems: DrawerItem[]; // Suponiendo que drawerItems es una matriz de objetos con propiedades label y path
+  drawerItemsRelevamientos: DrawerItem[]; // Suponiendo que drawerItemsRelevamientos es una matriz de objetos con propiedades label y path
+  drawerItemsRentas: DrawerItem[]; // Suponiendo que drawerItemsRentas es una matriz de objetos con propiedades label y path
+  drawerItemsAyuda: DrawerItem[]; // Suponiendo que drawerItemsAyuda es una matriz de objetos con propiedades label y path
+  iconMap: { [key: string]: React.ReactNode }; // Suponiendo que iconMap es un objeto que asigna etiquetas a iconos React
+}
 
 
 
@@ -48,78 +47,40 @@ export default function BotonHamburguesa() {
   const DrawerList = (
     <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
       <List>
-      <div className="layoutsidebarReportes">
-        {drawerItemsReportes.map((item, index) => (
-          <ListItem key={index} disablePadding >
-            <Link className="drawerItem" to={item.path} >
-              <ListItemButton className="navegadorSidebar">
-                <ListItemIcon sx={{ color: "var(--letrasSidebar)" }}>{iconMap[item.label]}</ListItemIcon>
-                <ListItemText primary={item.label} sx={{ color: "var(--letrasSidebar)"  }} />
-              </ListItemButton>
-            </Link>
-          </ListItem>
-        ))}
+        <div className="imagenPerfil">
+          <img src={perfil} alt="" />
+          <h1>Facundo Mangin</h1>
         </div>
 
         <Divider className="divider" />
 
         <div className="layoutsidebar">
-        {drawerItems.map((item, index) => (
-          <ListItem key={index} disablePadding>
-            <Link className="drawerItem" to={item.path} >
-              <ListItemButton className="navegadorSidebar">
-                <ListItemIcon sx={{ color: "var(--letrasSidebar)" }}>{iconMap[item.label]}</ListItemIcon>
-                <ListItemText primary={item.label} sx={{ color: "var(--letrasSidebar)" }} />
-              </ListItemButton>
-            </Link>
-          </ListItem>
-        ))}
+          {drawerItems.map((item, index) => (
+            <ListItem key={index} disablePadding>
+              <Link className="drawerItem" to={item.path} >
+                <ListItemButton className="navegadorSidebar">
+                  <ListItemIcon sx={{ color: "var(--letrasSidebar)" }}>{iconMap[item.label]}</ListItemIcon>
+                  <ListItemText primary={item.label} sx={{ color: "var(--letrasSidebar)" }} />
+                </ListItemButton>
+              </Link>
+            </ListItem>
+          ))}
         </div>
-
         <Divider className="divider" />
-
       </List>
 
-      <Divider className="divider" />
-
-      <div className="layoutsidebar">
-        {drawerItemsRentas.map((item, index) => (
-          <ListItem key={index} disablePadding>
-            <Link className="drawerItem" to={item.path} >
-              <ListItemButton className="navegadorSidebar">
-                <ListItemIcon sx={{ color: "var(--letrasSidebar)" }}>{iconMap[item.label]}</ListItemIcon>
-                <ListItemText primary={item.label} sx={{ color: "var(--letrasSidebar)"  }} />
-              </ListItemButton>
-            </Link>
-          </ListItem>
-        ))}
-        </div>
-
-        <Divider className="divider" />
 
 
-        <div className="layoutsidebar">
-        {drawerItemsAyuda.map((item, index) => (
-          <ListItem key={index} disablePadding>
-            <Link className="drawerItem" to={item.path} >
-              <ListItemButton className="navegadorSidebar">
-                <ListItemIcon sx={{ color: "var(--letrasSidebar)" }}>{iconMap[item.label]}</ListItemIcon>
-                <ListItemText primary={item.label} sx={{ color: "var(--letrasSidebar)" }} />
-              </ListItemButton>
-            </Link>
-          </ListItem>
-        ))}
-        </div>
     </Box>
   );
 
   return (
-    <div> 
-    <div><Button sx={{padding:'0.35rem', background:'#fff', borderRadius:'50%', minWidth:'30px'}} onClick={toggleDrawer(true) }> <MenuIcon sx={{color:'black', fontSize:'1.5rem'}}/> </Button>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        {DrawerList}
-      </Drawer></div>     
-      
-    </div>
+      <div className='BotonHamburguesa'>
+        <Button className='BotonHamburguesacolor' onClick={toggleDrawer(true)}> <MenuIcon sx={{ color: 'black', fontSize: '1.5rem' }} />
+        </Button>
+        <Drawer open={open} onClose={toggleDrawer(false)}>
+          {DrawerList}
+        </Drawer>
+      </div>
   );
 }
