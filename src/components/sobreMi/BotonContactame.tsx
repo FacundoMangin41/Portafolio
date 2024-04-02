a3f8346c8c19d5654ca3e4377a9004ae2926f6be
Facundo Mangin <63527441+FacundoMangin41@users.noreply.github.com>
2024-04-01 23:38:55 -0300

    arreglo de pagina sobre mi

---
+++ src/components/sobreMi/BotonContactame.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import './sobreMi.css'
+
+interface EmailButtonProps {
+    subject: string;
+    body: string;
+  }
+  
+  const BotonContactame: React.FC<EmailButtonProps> = ({ subject, body }) => {
+    const emailAddress = 'facundo.mangin@gmail.com';
+  
+    const handleButtonClick = () => {
+      const mailtoLink = `mailto:${emailAddress}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+      window.location.href = mailtoLink;
+    };
+  
+    return (
+      <Button variant="contained" className='botonSobreMi' onClick={handleButtonClick}>
+        Contacto
+      </Button>
+    );
+  };
+
+export default BotonContactame;
--- src/components/sobreMi/SobreMi.tsx
+++ src/components/sobreMi/SobreMi.tsx
@@ -2,80 +2,49 @@ import React, { useEffect, useRef } from 'react';
 import ScrollReveal from 'scrollreveal';
 import './sobreMi.css'
 import fotoPersonal from "../../assets/fotoPersonal.jpg";
+import BotonContactame from './BotonContactame';
 
 const SobreMi = () => {
 
     const elementRefs = useRef<(HTMLDivElement | null)[]>([]);
 
-  useEffect(() => {
-    elementRefs.current.forEach((element) => {
-        if (element) {
-          ScrollReveal().reveal(element, {
-            // Configuración de ScrollReveal
-            duration: 1000,
-            delay: 200,
-            distance: '20px',
-            origin: 'bottom'
-          });
-        }
-      });
+    useEffect(() => {
+        elementRefs.current.forEach((element) => {
+            if (element) {
+                ScrollReveal().reveal(element, {
+                    // Configuración de ScrollReveal
+                    duration: 1000,
+                    delay: 200,
+                    distance: '20px',
+                    origin: 'bottom'
+                });
+            }
+        });
     }, []);
 
     return (
         <div className='contenedorSobreMi' ref={(element) => { if (element) elementRefs.current.push(element); }}>
-            <h1>Sobre Mi</h1>
+            <h1 className='tituloSobreMi'>Sobre Mi</h1>
             <div className="imagenPersonal">
                 <img src={fotoPersonal} alt="" />
             </div>
-            <div className="informacionSobreMi">
-                <p>Actualmente estoy terminando la carrera de
-                    analista informática <em>(solo me faltan 2
-                    materias)</em> y me encuentro realizando una
-                    pagina web de gestion para el municipio de Baradero. <br />
-                    Tengo varios cursos realizados los cuales pueden encontrarlos en la sección de este portafolio. <br />
-                    Mi principal objetivo es poder
-                    desarrollarme en una empresa que me                 permita
-                    perfeccionar mis técnicas y conocimientos
-                    junto a otros profesionales.</p>
+            <div className="contenedorInformacionPersonal">
+                <h1>Sobre Mi</h1>
+                <div className="informacionSobreMi">
+                    <p>Actualmente estoy terminando la carrera de
+                        analista informática <em>(solo me faltan 2
+                            materias)</em> y me encuentro realizando una
+                        pagina web de gestion para el municipio de Baradero. <br />
+                        Tengo varios cursos realizados los cuales pueden encontrarlos en la sección de este portafolio. <br />
+                        Mi principal objetivo es poder
+                        desarrollarme en una empresa que me permita
+                        perfeccionar mis técnicas y conocimientos
+                        junto a otros profesionales. <br />
+                        Tambien soy un entusiasta del deporte, amante de los viajes, aficionado a los videojuegos y partidario de los encuentros familiares. </p>
+                        <BotonContactame subject={''} body={''} />
+                </div>
             </div>
 
-
-
-            {/* <div className="experienciaLaboral">
-                <h1>Experiencia Laboral</h1>
-
-                <div className="contenedoresEsperienciaLaboral">
-                    <div className="Trabajos">
-                        <h1>Freelance</h1>
-                        <h2>Desarrollador FrontEnd de Inove</h2>
-                        <p>Inicio: 2023 <br />
-                            Hasta: 2024</p>
-                        <h2>Desarrollo en:</h2>
-                        <li>HTML / CSS</li>
-                        <li>JavaScript</li>
-                        <li>ReactJS</li>
-                    </div>
-
-                    <div className="Trabajos">
-                        <h1>Municipalidad de Baradero</h1>
-                        <h2>Soporte IT</h2>
-                        <p>Inicio: 2022 <br />
-                            Hasta: Actualidad
-                        </p>
-                        <h2>Tareas realizadas:</h2>
-                        <li>Mantenimiento e instalación de equipos</li>
-                        <li>Armado de PC</li>
-                        <li>Instalación de Softwares</li>
-                        <li>Instalación de impresoras (LAN, USB, WiFi)</li>
-                        <li>Cableados de Ethernet y Fibra Óptica</li>
-                        <li>Organización de cableados</li>
-                        <li>Telefonía</li>
-
-                    </div>
-
-                </div>
-
-            </div> */}
         </div>
     );
 };
