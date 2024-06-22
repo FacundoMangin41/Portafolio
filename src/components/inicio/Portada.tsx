675bab406777aa671c3621f341f38c165c43b227
Facundo Mangin <63527441+FacundoMangin41@users.noreply.github.com>
2024-06-22 17:11:07 -0300

    agregado de tecnologias

--- src/components/inicio/Portada.tsx
+++ src/components/inicio/Portada.tsx
@@ -14,6 +14,22 @@ import cv from "../../assets/cv.jpg";
 import React, { useEffect, useRef } from 'react';
 import ScrollReveal from 'scrollreveal';
 
+import html5 from "../../assets/herramientas/html5.png";
+import css from "../../assets/herramientas/css3.png";
+import JavaScript from "../../assets/herramientas/javascript.png";
+import typescript from "../../assets/herramientas/typescript.png";
+import react from "../../assets/herramientas/react.png";
+import bootstrap from "../../assets/herramientas/bootstrap.png";
+import sass from "../../assets/herramientas/sass.png";
+import npm from "../../assets/herramientas/npm.png";
+import trello from "../../assets/herramientas/trello.png";
+import vscode from "../../assets/herramientas/vs-code.png";
+import materialUI from "../../assets/herramientas/materialUI.png";
+
+
+
+
+
 function handleClick() {
     try {
         const link = document.createElement('a');
@@ -49,7 +65,7 @@ const Portada = () => {
         <Box className='contenedorInicio'>
             <div className="informacionPortada">
 
-            <div className="imagenInicio" id='portadaFotoArriba' ref={(element) => { if (element) elementRefs.current.push(element); }}>
+                <div className="imagenInicio" id='portadaFotoArriba' ref={(element) => { if (element) elementRefs.current.push(element); }}>
                     <img src={perfil} alt="" />
                 </div>
 
@@ -66,6 +82,92 @@ const Portada = () => {
                                 <span> analista en informática</span>.
                                 Esto me permite diseñar interfaces interactivas y desarrollar soluciones informáticas eficientes"</h3>
                         </div>
+
+                        <div className="skillsPortada" id="skills" ref={(element) => { if (element) elementRefs.current.push(element); }}>
+                            <div className="skills-box" data-skills-box>
+
+                                <div className="skill-card">
+
+                                    <div className="card-icon">
+                                        <img src={html5} alt="HTML5 logo" />
+                                    </div>
+                                </div>
+
+                                <div className="skill-card">
+
+                                    <div className="card-icon">
+                                        <img src={css} alt="CSS3 logo" />
+                                    </div>
+                                </div>
+
+                                <div className="skill-card">
+
+                                    <div className="card-icon">
+                                        <img src={JavaScript} alt="JavaScript logo" />
+                                    </div>
+                                </div>
+
+                                <div className="skill-card">
+
+                                    <div className="card-icon">
+                                        <img src={typescript} alt="JavaScript logo" />
+                                    </div>
+                                </div>
+
+                                <div className="skill-card">
+
+                                    <div className="card-icon">
+                                        <img src={react} alt="JavaScript logo" />
+                                    </div>
+                                </div>
+
+                                <div className="skill-card">
+
+                                    <div className="card-icon">
+                                        <img src={bootstrap} alt="JavaScript logo" />
+                                    </div>
+                                </div>
+
+                                <div className="skill-card">
+
+                                    <div className="card-icon">
+                                        <img src={materialUI} alt="JavaScript logo" />
+                                    </div>
+                                </div>
+
+                                <div className="skill-card">
+
+                                    <div className="card-icon">
+                                        <img src={sass} alt="JavaScript logo" />
+                                    </div>
+                                </div>
+
+                                <div className="skill-card">
+
+                                    <div className="card-icon">
+                                        <img src={npm} alt="JavaScript logo" />
+                                    </div>
+                                </div>
+
+                                <div className="skill-card">
+
+                                    <div className="card-icon">
+                                        <img src={trello} alt="JavaScript logo" />
+                                    </div>
+                                </div>
+
+                                <div className="skill-card">
+
+                                    <div className="card-icon">
+                                        <img src={vscode} alt="JavaScript logo" />
+                                    </div>
+                                </div>
+
+
+                            </div>
+
+                        </div>
+
                         <div className="botonPortada" ref={(element) => { if (element) elementRefs.current.push(element); }}>
                             <Link to="/sobre-mi" style={{ textDecoration: 'none' }} >
                                 <Button endIcon={<SendIcon />} className='botonInicio'>
@@ -73,6 +175,7 @@ const Portada = () => {
                                 </Button>
                             </Link>
                         </div>
+
                     </div>
 
                     {/* <div className="imagenInicio" ref={(element) => { if (element) elementRefs.current.push(element); }}>
