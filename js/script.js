2dea10c4f0a88d0e258f6d0b8e5491091e692f5b
Facundo Mangin <63527441+FacundoMangin41@users.noreply.github.com>
2025-01-04 15:55:48 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Tecnico en Informatica',],
+    strings: ['Frontend Developer', 'Técnico en Informática',],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
